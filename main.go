b8df4519ec21616b244c5c3af96eb09a6202b26a
Kane York <rikingcoding@gmail.com>
2016-04-23 21:10:23 -0700

    initial code with healthcheck

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+	"log"
+)
+
+type stringWriter interface{
+	WriteString(s string) (n int, err error)
+}
+
+func HTTPHealthCheck(w http.ResponseWriter, r *http.Request) {
+	w.(stringWriter).WriteString("ok")
+}
+
+func main() {
+
+	http.HandleFunc("/healthcheck", HTTPHealthCheck)
+
+	err := http.ListenAndServe("127.0.0.1:2201", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
