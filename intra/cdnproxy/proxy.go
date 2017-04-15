a3951581eab746486a358fec3ffb6e207ea20770
Kane York <rikingcoding@gmail.com>
2017-04-15 01:45:21 -0700

    forgot to set expires on the dns cache

--- intra/cdnproxy/proxy.go
+++ intra/cdnproxy/proxy.go
@@ -31,6 +31,7 @@ func (c *customDNSCache) Get() (addrs []string, err error) {
 			return nil, err
 		}
 		c.res = res
+		c.expires = time.Now().Add(30*time.Minute)
 		return c.res, nil
 	}
 	return c.res, nil
